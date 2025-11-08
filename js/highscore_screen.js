13fc22009fdb7673499aa43feec035964eb1a0cf
AdityaDas5501 <adityadas5501@gmail.com>
2025-11-08 23:05:42 +0530

    Fixed Highscore List

--- js/highscore_screen.js
+++ js/highscore_screen.js
@@ -16,6 +16,13 @@ let rankHtml=`
 
 function rankList(){
     highscores = JSON.parse(localStorage.getItem('highscore'));
+    if(localStorage.getItem('highscore')===null){
+        rankHtml=`
+        <div class="grid-name" style="text-decoration: underline; margin-bottom: 30px">Name</div>
+        <div class="grid-score" style="text-decoration: underline;">Score</div>
+        <div class="grid-time" style="text-decoration: underline;">Time</div>
+        `;
+    }
     for (let i = 0; i < (highscores?.length || 0); i++) {
         rankHtml += `
             <div class="grid-name">${highscores[i].name}</div>
